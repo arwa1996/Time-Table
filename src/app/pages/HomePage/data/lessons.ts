cb8af650c8402341753f1f53f10aa1c6edd07770
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-02-19 15:53:52 +0200

    feat(lessons.ts): create all lessons query

---
+++ src/app/pages/HomePage/data/lessons.ts
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+
+export const GET_All_LESSONS = gql`
+  query GetAllLessons {
+    allLessons {
+      id
+      title
+      description
+      start
+      end
+    }
+  }
+`;
