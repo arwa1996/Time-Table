9d986568a549efa243eb69dfeab67c1c18c95e1b
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-02-22 23:44:45 +0200

    feat(formLessons): add validation to form fields

--- src/app/components/FormLessons.tsx
+++ src/app/components/FormLessons.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { useForm, Controller } from "react-hook-form";
 import { Event } from "react-big-calendar";
 import { Button, Form, Input } from "antd";
 
@@ -7,7 +6,9 @@ type FormProps = {
   start?: string | undefined;
   end?: string | undefined;
   selectedLesson?: Event;
-  onSubmit: (data: any) => void;
+  onSubmit: (data: {
+    lesson: { title: string; description: string; subject: string };
+  }) => void;
 };
 
 const layout = {
@@ -21,65 +22,70 @@ export const FormLessons: React.FC<FormProps> = ({
   end,
   onSubmit,
 }) => {
+  const [form] = Form.useForm();
   const lessonDefaultValues = {
     title: selectedLesson?.title || "",
     subject: selectedLesson?.resource.subject || "",
-    start: (selectedLesson?.start as string | undefined) || "",
-    end: (selectedLesson?.end as string | undefined) || "",
+    start: (selectedLesson?.start as string | undefined) || start,
+    end: (selectedLesson?.end as string | undefined) || end,
     description: selectedLesson?.resource.description || "",
   };
 
-  const { setValue, handleSubmit, control, reset } = useForm<FormProps>({
-    defaultValues: lessonDefaultValues,
-  });
-
   useEffect(() => {
-    reset(lessonDefaultValues);
+    form.resetFields();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedLesson, start, end]);
 
   return (
-    <Form {...layout} name="nest-messages" onFinish={handleSubmit(onSubmit)}>
+    <Form {...layout} name="control-hooks" form={form} onFinish={onSubmit}>
       <Form.Item
         name={["lesson", "title"]}
         label="lesson Title"
+        rules={[
+          {
+            required: true,
+            message: "This field is required",
+          },
+          {
+            min: 3,
+            pattern: /^[A-Za-z0-9 ]+$/i,
+            message: "Please enter a valid lesson title",
+          },
+          { max: 100, message: "Maximum characters allowed is 100" },
+
+          { min: 3, message: "Minimum characters allowed is 3 " },
+        ]}
         initialValue={lessonDefaultValues.title}
       >
-        <Controller
-          render={({ value, onChange }) => (
-            <Input value={value} onChange={onChange} />
-          )}
-          control={control}
-          name={"title"}
-          rules={{ required: true }}
-        />
+        <Input />
       </Form.Item>
       <Form.Item
         name={["lesson", "subject"]}
         label="Subject"
+        rules={[
+          {
+            required: true,
+            message: "This field is required",
+          },
+          {
+            min: 3,
+            pattern: /^[A-Za-z0-9 ]+$/i,
+            message: "Please enter a valid lesson title",
+          },
+          { max: 100, message: "Maximum characters allowed is 100" },
+
+          { min: 3, message: "Minimum characters allowed is 3 " },
+        ]}
         initialValue={lessonDefaultValues.subject}
       >
-        <Controller
-          render={({ value, onChange }) => (
-            <Input value={value} onChange={onChange} />
-          )}
-          name="subject"
-          control={control}
-          rules={{ required: true }}
-        />
+        <Input />
       </Form.Item>
       <Form.Item
         name={["lesson", "start"]}
         label="Start"
         initialValue={lessonDefaultValues.start}
       >
-        <Controller
-          render={() => (
-            <Input value={start} onChange={() => setValue("start", start)} />
-          )}
-          name="start"
-          control={control}
-        />
+        <Input disabled />
       </Form.Item>
 
       <Form.Item
@@ -87,26 +93,14 @@ export const FormLessons: React.FC<FormProps> = ({
         label="End"
         initialValue={lessonDefaultValues.end}
       >
-        <Controller
-          render={() => (
-            <Input value={end} onChange={() => setValue("end", end)} />
-          )}
-          name="end"
-          control={control}
-        />
+        <Input disabled />
       </Form.Item>
       <Form.Item
         name={["lesson", "description"]}
         label="Description"
         initialValue={lessonDefaultValues.description}
       >
-        <Controller
-          render={({ value, onChange }) => (
-            <Input.TextArea value={value} onChange={onChange} />
-          )}
-          name="description"
-          control={control}
-        />
+        <Input.TextArea />
       </Form.Item>
       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 11 }}>
         <Button type="primary" htmlType="submit">
